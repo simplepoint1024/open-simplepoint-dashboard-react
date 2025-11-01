0ebc1ebf5d6424b933ad21e7b7d0baa9674031a2
刘金旭 <somesimpled@gmail.com>
2025-11-02 00:02:20 +0800

    chore: change directory structure

--- mocks/index.ts
+++ mocks/index.ts
@@ -1,7 +1,7 @@
 import {setupWorker} from 'msw/browser';
 
-// 聚合 modules/**/mocks/*.ts 中导出的 handlers
-const ctx = require.context('../modules', true, /mocks\/.*\.ts$/);
+// 聚合 apps/**/mocks/*.ts 中导出的 handlers
+const ctx = require.context('../apps', true, /mocks\/.*\.ts$/);
 const handlers = ctx.keys().flatMap((key: any) => {
   const mod = ctx(key) as any;
   // 兼容多种导出方式：默认导出数组、命名导出 apis、默认导出对象.apis
